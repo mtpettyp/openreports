ee6b6b6f0666ddc06a5a9668e2b9e3de3e4a4077
eswenson <eswenson@35531a67-7d2f-0410-81a2-b8b02ed50f64>
2007-11-27 03:47:51 +0000

    added openreports example database and OLAP and Velocity examples
    git-svn-id: svn://svn.code.sf.net/p/oreports/code/trunk@27 35531a67-7d2f-0410-81a2-b8b02ed50f64

---
+++ openreports/database/start-database.bat
@@ -0,0 +1,1 @@
+java -classpath hsql/hsqldb-1.7.3.jar org.hsqldb.Server -database.0 ./hsql/openreports -dbname.0 openreports -database.1 ./hsql/sample -dbname.1 sample -port 9001 -silent true -trace false
