3824dfb724504a16969302e618a91328c0cdb415
Mike Pettypiece <mike@streetcontxt.com>
2018-11-13 13:14:57 -0500

    Fix issue with dates
    After the upgrade to Struts 2.3, the string posted from a
    datetimepicker is in ISO-8601 format, rather than the "displayFormat"

--- src/main/java/org/efs/openreports/providers/DateProvider.java
+++ src/main/java/org/efs/openreports/providers/DateProvider.java
@@ -1,54 +1,50 @@
 /*
  * Copyright (C) 2002 Erik Swenson - erik@oreports.com
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
  * Software Foundation; either version 2 of the License, or (at your option)
  * any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
  * more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with
  * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
  * Place - Suite 330, Boston, MA 02111-1307, USA.
- *  
+ *
  */
 
 package org.efs.openreports.providers;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.time.format.DateTimeFormatter;
+import java.time.Instant;
 
 import org.apache.log4j.Logger;
 import org.efs.openreports.objects.ORProperty;
 
-public class DateProvider 
-{
-	protected static Logger log =
-		Logger.getLogger(DateProvider.class.getName());
+public class DateProvider {
+	protected static Logger log = Logger.getLogger(DateProvider.class.getName());
 
 	protected SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
 
 	private PropertiesProvider propertiesProvider;
-	
-	public DateProvider(PropertiesProvider propertiesProvider) throws ProviderException
-	{
-		this.propertiesProvider = propertiesProvider;			
+
+	public DateProvider(PropertiesProvider propertiesProvider) throws ProviderException {
+		this.propertiesProvider = propertiesProvider;
 		init();
-	}	
-	
-	protected void init() throws ProviderException
-	{
+	}
+
+	protected void init() throws ProviderException {
 		String dateFormat = "MM/dd/yyyy";
 
-		ORProperty property = propertiesProvider.getProperty(ORProperty.DATE_FORMAT);		
-		if (property != null && property.getValue() != null
-				&& property.getValue().trim().length() > 0)
-		{
-			dateFormat = property.getValue(); 
+		ORProperty property = propertiesProvider.getProperty(ORProperty.DATE_FORMAT);
+		if (property != null && property.getValue() != null && property.getValue().trim().length() > 0) {
+			dateFormat = property.getValue();
 		}
 
 		setDateFormat(dateFormat);
@@ -56,36 +52,28 @@ public class DateProvider
 		log.info("DateFormat: " + dateFormat);
 		log.info("Created");
 	}
-	
-	public SimpleDateFormat getDateFormat()
-	{
+
+	public SimpleDateFormat getDateFormat() {
 		return dateFormat;
 	}
 
-	public void setDateFormat(String format)
-	{
+	public void setDateFormat(String format) {
 		dateFormat = new SimpleDateFormat(format);
 	}
 
-	public Date parseDate(String date) throws ProviderException
-	{
-		try
-		{
-			return dateFormat.parse(date);
-		}
-		catch (Exception e)
-		{
+	public Date parseDate(String date) throws ProviderException {
+		try {
+			return Date.from(Instant.from(DateTimeFormatter.ISO_DATE_TIME.parse(date)));
+		} catch (Exception e) {
 			throw new ProviderException("Use " + dateFormat.toPattern());
 		}
 	}
-	
-	public String formatDate(Date date) 
-	{		
-		return dateFormat.format(date);		
-	}	
 
-	public void setPropertiesProvider(PropertiesProvider propertiesProvider)
-	{
+	public String formatDate(Date date) {
+		return dateFormat.format(date);
+	}
+
+	public void setPropertiesProvider(PropertiesProvider propertiesProvider) {
 		this.propertiesProvider = propertiesProvider;
 	}
 
